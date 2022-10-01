23cd3e36016ad7ec1182fd984e6abcfeb36a0491
Nikita Ovsyannikov <nik_da_2000@mail.ru>
2022-10-01 23:15:13 +0300

    Adding basic methods of the storage structure

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,13 +1,13 @@
 package storage
 
 import (
-	"avito-backend-trainee/internal/server"
 	"context"
 	"errors"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/log/zapadapter"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/shopspring/decimal"
 	"go.uber.org/zap"
 )
 
@@ -47,8 +47,115 @@ func (s *Storage) Close() {
 	s.db.Close()
 }
 
-func (s *Storage) AddNewAd(ctx context.Context, values server.NewAdRequest) (int64, error) {
+func (s *Storage) AddNewAd(ctx context.Context, name string, description string, links []string, price decimal.Decimal) (int64, error) {
 	logger := s.logger.With()
 	logger.Debug("add new ad to table")
 
+	var id int64
+
+	err := s.db.QueryRow(
+		ctx,
+		firstIsertExec,
+		name,
+		description,
+		links,
+		price,
+	).Scan(id)
+	if err != nil {
+		//
+		return 0, err
+	}
+	return id, err
+}
+
+func (s *Storage) ReadAllAds(ctx context.Context, order *OrdBy, offset int64, gradation PaginationFields) ([]GetAdResponse, error) {
+	logger := s.logger.With()
+	logger.Debug("")
+
+	var newQuery string
+	var newRows pgx.Rows
+
+	if order != nil {
+		switch gradation {
+		case Ascending:
+			newQuery = readTableOrder
+		case Descending:
+			newQuery = readTableOrderDesc
+		}
+		rows, err := s.db.Query(
+			ctx,
+			newQuery,
+			*order,
+			Limit,
+			offset,
+		)
+		if err != nil {
+			//
+			return nil, err
+		}
+		newRows = rows
+	} else {
+		newQuery = readTable
+		rows, err := s.db.Query(
+			ctx,
+			newQuery,
+			Limit,
+			offset,
+		)
+		if err != nil {
+			//
+			return nil, err
+		}
+		newRows = rows
+	}
+
+	var dd []GetAdResponse
+	for newRows.Next() {
+		var d GetAdResponse
+		err := newRows.Scan(&d.Name, &d.Links, &d.Price)
+		if err != nil {
+			//
+			return nil, err
+		}
+		dd = append(dd, d)
+	}
+	return dd, nil
+}
+
+func (s *Storage) ReadAd(ctx context.Context, id int64, fields ...AdditionalFields) (GetAdResponse, error) {
+	logger := s.logger.With()
+	logger.Debug("")
+
+	var userAd GetAdResponse
+
+	var readTable string
+
+	var total AdditionalFields
+
+	for _, field := range fields {
+		total = total | field
+	}
+
+	switch total {
+	case total & 0:
+		readTable = readNoFields
+	case total & Description:
+		readTable = readNoLinks
+	case total & Links:
+		readTable = readNoDescription
+	case total & (Description | Links):
+		readTable = readAllFields
+	}
+
+	err := s.db.QueryRow(
+		ctx,
+		readTable,
+		id,
+	).Scan(&userAd.Name, &userAd.Description, &userAd.Links, &userAd.Price)
+	if err != nil {
+		//
+		return GetAdResponse{}, err
+	}
+
+	return userAd, nil
 }
