92dde6f433d70eeffd719572384bd6ffaec4e46c
Ovsyannikov Nikita <nik_da_2000@mail.ru>
2022-10-05 20:23:31 +0300

    Changes: Building a development logger Loading env fiale

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"avito-backend-trainee/internal/server"
+	"avito-backend-trainee/internal/storage"
+	"context"
+	"log"
+
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("zap.NewDevelopment: %v", err)
+	}
+	defer logger.Sync()
+
+	if err := godotenv.Load("../../.env"); err != nil {
+		logger.Debug("No .env file found", zap.Error(err))
+	}
+
+	ctx := context.Background()
+
+	storage, err := storage.NewStorage(ctx, logger)
+	if err != nil {
+		logger.Fatal("failed to create storage instance", zap.Error(err))
+	}
+
+	srv, err := server.Server(
+		logger,
+		storage,
+		storage.Close,
+	)
+
+	if err != nil {
+		logger.Fatal("failed to create http server instance", zap.Error(err))
+	}
+
+	err = srv.Start()
+	if err != nil {
+		logger.Fatal("failed to start or shutdown server", zap.Error(err))
+	}
+}
